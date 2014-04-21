d2dd9c8a8a869e7fa7db91c49972be193c884f7c
Pierre Malhaire <pierre.malhaire@gmail.com>
2014-04-21 20:32:14 +0200

    respace and clean javascript

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,42 +1,39 @@
-window.onload = function()
-{
-	document.getElementById("list_id").onclick = function()
-	{
-            //set entry dir
-            var entry="/";
+window.onload = function () {
+    document.getElementById("list_id").onclick = function () {
+        //set entry dir
+        var entry = "/";
+        var listFiles,listDirs = [];
+        var obj = "<ul class=\"fileTree\">";
 
-            //disable button to avoid mutliple calls
-            document.getElementById("list_id").disabled = true;
-
-            var listFiles = [];
-            var listDirs = [];
-            listFiles = Lima.listFiles(entry);
-            listDirs = Lima.listDirs(entry);
-			var obj = "<ul class=\"fileTree\">";
-			if(	listDirs )
-			for(var i = 0; i < listDirs.length; i++)
-			{
-				obj=obj.concat("<li class=\"directory\">");
-				obj=obj.concat(listDirs[i]);
-				obj=obj.concat("</li>");
-			}
-			if(	listFiles )
-            for(var j = 0; j < listFiles.length; j++)
-			{
-				obj=obj.concat("<li class=\"file\">");
-                obj=obj.concat(listFiles[j]);
-				obj=obj.concat("</li>");
-			}
-			obj=obj.concat("</ul>");
-			document.getElementById("container_id").innerHTML=obj;
+        //disable button to avoid mutliple calls
+        document.getElementById("list_id").disabled = true;
 
 
-            delete listDirs;
-            delete listFiles;
-	};
-	document.getElementById("exit_id").onclick = function()
-	{
-         Lima.quit();
-	};
+        listFiles = Lima.listFiles(entry);
+        listDirs = Lima.listDirs(entry);
+
+        if (listDirs) {
+            for (var i = 0; i < listDirs.length; i++) {
+                obj = obj.concat("<li class=\"directory\">");
+                obj = obj.concat(listDirs[i]);
+                obj = obj.concat("</li>");
+            }
+        }
+        if (listFiles) {
+            for (var j = 0; j < listFiles.length; j++) {
+                obj = obj.concat("<li class=\"file\">");
+                obj = obj.concat(listFiles[j]);
+                obj = obj.concat("</li>");
+            }
+        }
+        obj = obj.concat("</ul>");
+        document.getElementById("container_id").innerHTML=obj;
+
+        delete listDirs;
+        delete listFiles;
+    };
+    document.getElementById("exit_id").onclick = function () {
+        Lima.quit();
+    };
 }
 
