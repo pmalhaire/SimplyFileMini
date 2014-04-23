4f1ef7cc85fc3ed3f7e959eb857944bb789ac31b
Pierre Malhaire <pierre.malhaire@gmail.com>
2014-04-23 09:25:45 +0200

    Added test on dir existance

--- controleur.cpp
+++ controleur.cpp
@@ -7,43 +7,43 @@
 
 bool Controleur::listDirs(QString isDir, QStringList &ioDirList)
 {
-    try{
-        QDirIterator dirIt(isDir,
-                            QStringList() << "*",
-                            QDir::NoSymLinks | QDir::Dirs | QDir::NoDotAndDotDot
-                            );
-        while (dirIt.hasNext()) {
-            dirIt.next();
-            if (QFileInfo(dirIt.filePath()).isDir())
-            {
-                ioDirList.append(dirIt.fileName());
-            }
-        }
-    }catch (QException &e){
-        qDebug() << e.what();
+    if(!QDir(isDir).exists())
+    {
+        qDebug() << isDir << "don't exists";
         return false;
     }
+    QDirIterator dirIt(isDir,
+                       QStringList() << "*",
+                       QDir::NoSymLinks | QDir::Dirs | QDir::NoDotAndDotDot
+                       );
 
+    while (dirIt.hasNext()) {
+        dirIt.next();
+        if (QFileInfo(dirIt.filePath()).isDir())
+        {
+            ioDirList.append(dirIt.fileName());
+        }
+    }
     return true;
 }
 
 
 bool Controleur::listFiles(QString isDir, QStringList &ioFileList){
-    try{
-        QDirIterator fileIt(isDir,
-                             QStringList() << "*",
-                             QDir::NoSymLinks | QDir::Files | QDir::NoDotAndDotDot
-                             );
-        while (fileIt.hasNext()) {
-            fileIt.next();
-            if (QFileInfo(fileIt.filePath()).isFile())
-            {
-                ioFileList.append(fileIt.fileName());
-            }
-        }
-    }catch (QException &e){
-        qDebug() << e.what();
+    if(!QDir(isDir).exists())
+    {
+        qDebug() << isDir << "don't exists";
         return false;
+    }
+    QDirIterator fileIt(isDir,
+                        QStringList() << "*",
+                        QDir::NoSymLinks | QDir::Files | QDir::NoDotAndDotDot
+                        );
+    while (fileIt.hasNext()) {
+        fileIt.next();
+        if (QFileInfo(fileIt.filePath()).isFile())
+        {
+            ioFileList.append(fileIt.fileName());
+        }
     }
     return true;
 }
